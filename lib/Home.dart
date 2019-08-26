fbb05fbc3504789ff5d75be1d6a0e5038d813879
Kuangzhongwen <651043704@qq.com>
2019-08-26 10:52:32 +0800

    首页 icon

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/widgets.dart';
+import 'icon_item.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -8,6 +9,24 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<ServiceItemViewModel> iconList = [
+    ServiceItemViewModel(title: '全职招聘', icon: _image("publish_icon_house")),
+    ServiceItemViewModel(
+        title: '简直', icon: _image("publish_icon_house_zufang")),
+    ServiceItemViewModel(
+        title: '租房', icon: _image("publish_icon_qiuzuqiugouqiufuwu")),
+    ServiceItemViewModel(title: '二手房', icon: _image("publish_icon_sale")),
+    ServiceItemViewModel(title: '二手物品', icon: _image("publish_icon_zhaopin"))
+  ];
+
+  static Widget _image(String iconName) {
+    return Image.asset(
+      'images/${iconName}.png',
+      width: 60,
+      height: 60,
+    );
+  }
+
   @override
   void initState() {
     super.initState();
@@ -22,26 +41,12 @@ class _HomeState extends State<Home> {
         new Image.asset(
           'images/ic_home_banner.png',
         ),
-        new Row(
-          children: [
-            new Image.asset(
-              'images/publish_icon_zhaopin.png',
-            ),
-            new Image.asset(
-              'images/publish_icon_qiuzuqiugouqiufuwu.png',
-            ),
-            new Image.asset(
-              'images/publish_icon_house_zufang.png',
-            ),
-            new Image.asset(
-              'images/publish_icon_house.png',
-            ),
-            new Image.asset(
-              'images/publish_icon_sale.png',
-            )
-          ],
-        )
-      ],
+//        new GridView.count(
+//          crossAxisCount: 5,
+//          padding: EdgeInsets.symmetric(vertical: 0),
+//          children: iconList.map((item) => IconItem(data: item)).toList(),
+//        )
+      ]
     );
   }
 }
