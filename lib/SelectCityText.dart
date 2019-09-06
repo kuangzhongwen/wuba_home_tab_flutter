3e632a65472bbe99f1318269bac80ad04c5c2b23
Kuangzhongwen <651043704@qq.com>
2019-09-06 18:13:00 +0800

    城市 text widget 优化

--- lib/SelectCityText.dart
+++ lib/SelectCityText.dart
@@ -1,9 +1,7 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SelectCityText extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
     return _SelectCityState();
@@ -11,21 +9,26 @@ class SelectCityText extends StatefulWidget {
 }
 
 class _SelectCityState extends State<SelectCityText> {
-
   String selectCity = '北京';
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
-      child: Text(
-          selectCity,
-          textAlign: TextAlign.left,
-          style: TextStyle(
-              fontSize: 18.0,
-              color: Colors.white
-          )
-      )
-    );
+    return Row(children: <Widget>[
+      Padding(
+          padding: const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+          child: GestureDetector(
+              onTap: () {
+                print("onTap city text.");
+              },
+              child: Text(
+                selectCity,
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 18.0, color: Colors.white),
+              ))),
+      Padding(
+          padding: const EdgeInsets.fromLTRB(5, 0.0, 0.0, 0.0),
+          child: Image.asset("images/ic_location_down_white.png",
+              width: 13, height: 13))
+    ]);
   }
 }
