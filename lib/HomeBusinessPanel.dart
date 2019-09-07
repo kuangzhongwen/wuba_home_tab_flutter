20136c4e9ed6f9b83363966cad31f31cacba4156
Kuangzhongwen <651043704@qq.com>
2019-09-07 12:54:42 +0800

    首页输入框

--- lib/HomeBusinessPanel.dart
+++ lib/HomeBusinessPanel.dart
@@ -1,8 +1,7 @@
-
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class HomeBussinessPanel extends StatefulWidget {
-
   @override
   State createState() {
     return _HomeBussinessState();
@@ -11,8 +10,36 @@ class HomeBussinessPanel extends StatefulWidget {
 
 class _HomeBussinessState extends State<HomeBussinessPanel> {
 
+  String searchKey = '';
+
   @override
   Widget build(BuildContext context) {
-    return null;
+    return Container(
+      padding: const EdgeInsets.all(12.0),
+      child: Column(
+        children: <Widget>[
+          TextField(
+              decoration: new InputDecoration(
+                hintText: '房产',
+                prefixIcon: Icon(Icons.search, color: Colors.deepOrange),
+                contentPadding: const EdgeInsets.symmetric(vertical: 1.0),
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(6),
+                    borderSide: BorderSide(
+                      color: Color(0xffc7c7cc),
+                      width: 1.0
+                    ),
+                  gapPadding: 0.2,
+                ),
+              ),
+              style: new TextStyle(
+                  fontSize: 16.0,
+                  height: 2.0,
+                  color: Color(0xff666666)
+              )
+          )
+        ],
+      ),
+    );
   }
 }
